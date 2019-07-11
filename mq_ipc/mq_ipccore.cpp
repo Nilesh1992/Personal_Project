131002d95d5a31321e78753c96162034cdace9dd
Nilesh Gharde <ngharde@qti.qualcomm.com>
2019-07-11 23:15:07 +0530

    Added a seperate ipcore header and mq_close imple
    added code for mq_close and added a seperate hwader file
    for mq_coreipc

--- mq_ipc/mq_ipccore.cpp
+++ mq_ipc/mq_ipccore.cpp
@@ -1,82 +1,51 @@
-#include<iostream>
-#include<string.h>
-#ifdef __cplusplus
-extern "C" {
-#endif
-#define MAX_QUEUE_LENGTH 1000
-#define MAX_MSG_SIZE 300
-#include <mqueue.h>
-#include <fcntl.h>           /* For O_* constants */
-#include <sys/stat.h>
-#include <errno.h>
-    /* For mode constants */
-#ifdef __cplusplus
-}
-#endif
-using namespace std;
+#include "mq_ipccore.h"
+using namespace mq_ipccore;
 
-namespace mq_ipccore {
-    class IpcCore {
-        private:
-            mqd_t queue_id;
-            int error_code;
-            struct mq_attr default_attrs;
-        public:
-            IpcCore():queue_id(-1),error_code(0){
-                memset(&default_attrs,0,sizeof(default_attrs)); 
-                default_attrs.mq_maxmsg = 10;
-                default_attrs.mq_msgsize = 100;
-            };
-            mqd_t createMq(const char* name, int create_flags, mode_t mode, 
-                    struct mq_attr *attrs);
-            //int destroyMq();
-            //int subscribeToMq();
-            //int unsubscribeToMq();
-            //virtual bool sendToMq();
-            //virtual bool recvFromMq();
-    };
-
-    mqd_t IpcCore::createMq(const char* name, int create_flags, mode_t mode = O_RDONLY, 
-            struct mq_attr *attrs = NULL)
+mqd_t IpcCore::createMq(const char* name, int create_flags, mode_t mode = O_RDONLY, 
+        struct mq_attr *attrs = NULL)
+{
+    if(name == NULL || create_flags <= 0)
     {
-         if(name == NULL || create_flags <= 0)
-        {
-            cout << "Something went wrong!!!" << endl;
-            return -1;
-        }
-        if(NULL == attrs)
-        {
-            attrs = &default_attrs;
-        }
+        cout << "Something went wrong!!!" << endl;
+        return -1;
+    }
 
-        
-       if ((create_flags & O_CREAT) > 0)
-        {
-            queue_id = mq_open(name, create_flags, mode, attrs); 
-            error_code = errno;
-            cout << "Error received --->" << error_code << endl;
-        }
-            return queue_id; 
-    } 
+    if(NULL == attrs)
+    {
+        attrs = &default_attrs;
+    }
+
+    if ((create_flags & O_CREAT) > 0)
+    {
+        queue_id = mq_open(name, create_flags, mode, attrs); 
+    }
+    return queue_id; 
+} 
+
+int IpcCore::destroyMq(mqd_t queue_id)
+{
+    return mq_close(queue_id);
 }
-
 
 int main()
 {
-    mqd_t queue_id = -1 ;
+        mqd_t queue_id = -1 ;
     char name[] = "This is nilesh string one";
-     char name1[] = "This is nilesh stting two";
-     char receiver[MAX_QUEUE_LENGTH];
-    mq_ipccore::IpcCore *mq_sender_client = new mq_ipccore::IpcCore();
-    queue_id = mq_sender_client->createMq("/Niles", O_CREAT|O_RDWR, 0644);
-   cout << mq_send(queue_id, name, sizeof(name), 0) << endl; 
-    queue_id = mq_sender_client->createMq("/Niles", O_CREAT|O_RDWR, 0644);
-   cout << mq_send(queue_id, name1, sizeof(name), 0) << endl; 
+    char name1[] = "This is nilesh stting two";
+    char receiver[MAX_QUEUE_LENGTH];
+    IpcCore *mq_sender_client = new mq_ipccore::IpcCore();
+    queue_id = mq_sender_client->createMq("/Niles", O_CREAT|O_WRONLY, 0644);
+    cout << mq_send(queue_id, name, sizeof(name), 0) << endl;
+    cout << "Closing the connection" << mq_sender_client->destroyMq(queue_id)<<endl; 
+    queue_id = mq_sender_client->createMq("/Niles", O_CREAT|O_WRONLY, 0644);
+    cout << mq_send(queue_id, name1, sizeof(name), 0) << endl; 
+    cout << "Closing the connection" << mq_sender_client->destroyMq(queue_id)<<endl; 
     queue_id = mq_sender_client->createMq("/Niles", O_CREAT|O_RDONLY, 0644);
     cout << queue_id << endl;
-   cout << mq_receive(queue_id, receiver, MAX_QUEUE_LENGTH, 0) << endl; 
-   cout << "Received data is  --> " << receiver << endl;
-   cout << mq_receive(queue_id, receiver, MAX_QUEUE_LENGTH, 0) << endl; 
-   cout << "Received data is  --> " << receiver << endl;
+    cout << mq_receive(queue_id, receiver, MAX_QUEUE_LENGTH, 0) << endl; 
+    cout << "Received data is  --> " << receiver << endl;
+    cout << mq_receive(queue_id, receiver, MAX_QUEUE_LENGTH, 0) << endl; 
+    cout << "Received data is  --> " << receiver << endl;
+    cout << "Closing the connection" << mq_sender_client->destroyMq(queue_id)<<endl; 
     return 0;
 }
---
+++ mq_ipc/mq_ipccore.h
@@ -0,0 +1,47 @@
+/*************************i************
+//Bottom line is 
+// A given process should one its process queue in readonly mode
+// If a given process want to send any hing to other process the it should open 
+// the other process queue in write only mode
+ ***********************************/
+#include<iostream>
+#include<string.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define MAX_QUEUE_LENGTH 1000
+#define MAX_MSG_SIZE 300
+#include <mqueue.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>
+#include <errno.h>
+    /* For mode constants */
+#ifdef __cplusplus
+}
+#endif
+using namespace std;
+
+namespace mq_ipccore {
+    class IpcCore {
+        private:
+            mqd_t queue_id;
+            int error_code;
+            struct mq_attr default_attrs;
+        public:
+            IpcCore():queue_id(-1),error_code(0){
+                memset(&default_attrs,0,sizeof(default_attrs)); 
+                default_attrs.mq_maxmsg = 10;
+                default_attrs.mq_msgsize = 100;
+            };
+            mqd_t createMq(const char* name, int create_flags, mode_t mode, 
+                    struct mq_attr *attrs);
+            int destroyMq(mqd_t);
+            //int subscribeToMq();
+            //int unsubscribeToMq();
+            //virtual bool sendToMq();
+            //virtual bool recvFromMq();
+    };
+}
+
+
+
